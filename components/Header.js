de43f180531c022e4f28fc65baaf18685d9a5c20
Standard <standard@Aristotle-2.local>
2021-08-03 00:07:38 -0700

    updated logo and heading titles

--- components/Header.js
+++ components/Header.js
@@ -1,33 +1,31 @@
-import Image from "next/image";
-import HeaderItem from "./HeaderItem";
+import Image from 'next/image'
+import HeaderItem from './HeaderItem'
 import {
-    BadgeCheckIcon,
-    CollectionIcon,
-    HomeIcon,
-    LightningBoltIcon,
-    SearchIcon,
-    UserIcon,
-
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
 } from '@heroicons/react/outline'
 
-export default function Header () {
-    return (
-<header className ="flex flex-col sm:flex-row m-5 justify-between items-center h-auto ">
-<div className=" flex flex-grow justify-evenly max-w-2xl">
-<HeaderItem title="Home" Icon={HomeIcon} />
-<HeaderItem title="Home" Icon={LightningBoltIcon} />
-<HeaderItem title="Home" Icon={BadgeCheckIcon} />
-<HeaderItem title="Home" Icon={CollectionIcon} />
-<HeaderItem title="Home" Icon={SearchIcon} />
-<HeaderItem title="Home" Icon={UserIcon} />
-</div>
-<Image className="object-contain" 
-src = "https://links.papareact.com/ua6" 
-width={200} 
-height={100}
-/>
-</header>
-    )
+export default function Header() {
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto ">
+      <div className=" flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="Home" Icon={HomeIcon} />
+        <HeaderItem title="Trending" Icon={LightningBoltIcon} />
+        <HeaderItem title="Favorite" Icon={BadgeCheckIcon} />
+        <HeaderItem title="Collection" Icon={CollectionIcon} />
+        <HeaderItem title="Search" Icon={SearchIcon} />
+        <HeaderItem title="Profile" Icon={UserIcon} />
+      </div>
+      <Image
+        className="object-contain"
+        src="/Gamer X Society Website Layout-4.png"
+        width={200}
+        height={100}
+      />
+    </header>
+  )
 }
-
-
