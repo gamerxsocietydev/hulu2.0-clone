7dbf16ca716663a191139a07cd18c639499f899c
Standard <standard@Aristotle-2.local>
2021-08-02 23:51:12 -0700

    removed theme imports

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,6 @@ import PropTypes from 'prop-types'
 import Head from 'next/head'
 // import { ThemeProvider } from '@material-ui/core/styles';
 // import CssBaseline from '@material-ui/core/CssBaseline';
-// import theme from './src/theme';
 import 'tailwindcss/tailwind.css'
 
 import '../styles/global.css'
