ea095c153bb960cfef6b3c2145bb4bd1022c4f91
Standard <standard@Aristotle-2.local>
2021-08-02 23:43:06 -0700

    removed React import

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,13 @@
-import React from 'React'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import * as gtag from '../lib/gtag'
 
-import PropTypes from 'prop-types';
-import Head from 'next/head';
+import PropTypes from 'prop-types'
+import Head from 'next/head'
 // import { ThemeProvider } from '@material-ui/core/styles';
 // import CssBaseline from '@material-ui/core/CssBaseline';
 // import theme from './src/theme';
 import 'tailwindcss/tailwind.css'
-
-
 
 import '../styles/global.css'
 
@@ -28,19 +25,16 @@ function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector('#jss-server-side')
     if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
+      jssStyles.parentElement.removeChild(jssStyles)
     }
-  }, []);
-  return (
-      
-        <Component {...pageProps} />
-  )
+  }, [])
+  return <Component {...pageProps} />
 }
 
 MyApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object.isRequired,
-};
+}
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
-import React from 'React'
+import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheets } from '@material-ui/core/styles';
-import theme from './src/theme';
+import { ServerStyleSheets } from '@material-ui/core/styles'
+import theme from './src/theme'
 
 import { GA_TRACKING_ID } from '../lib/gtag'
 
@@ -44,7 +44,6 @@ export default class MyDocument extends Document {
   }
 }
 
-
 // `getInitialProps` belongs to `_document` (instead of `_app`),
 // it's compatible with server-side generation (SSG).
 MyDocument.getInitialProps = async (ctx) => {
@@ -71,19 +70,22 @@ MyDocument.getInitialProps = async (ctx) => {
   // 4. page.render
 
   // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
 
   ctx.renderPage = () =>
     originalRenderPage({
       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    });
+    })
 
-  const initialProps = await Document.getInitialProps(ctx);
+  const initialProps = await Document.getInitialProps(ctx)
 
   return {
     ...initialProps,
     // Styles fragment is rendered after the app and page rendering finish.
-    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
-  };
-};
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  }
+}
